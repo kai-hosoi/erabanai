fea5dfe007d0a573622bfd0aa3464f6ac6d6c63b
kai-hosoi <79440232+kai-hosoi@users.noreply.github.com>
2021-06-10 18:50:05 +0900

    書式を合わせるため、first→[0]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,7 @@ class SearchController < ApplicationController
     else
       style = URI.encode_www_form({private_room:1})
     end
+
     if params[:age] == "10" 
       if params[:personality] == "1"
           genre = "G007"#中華
@@ -44,10 +45,10 @@ class SearchController < ApplicationController
       uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=29952817473475a8&large_area=#{area}&budget=#{budget}&#{style}&genre=#{genre}")
       xml = Net::HTTP.get(uri)
       doc = Nokogiri::XML(xml)
-      @name1 = doc.css("shop").first.css("> name").first.content
+      @name1 = doc.css("shop")[0].css("> name").first.content
       @name2 = doc.css("shop")[1].css("name").first.content
       @name3 = doc.css("shop")[2].css("name").first.content
-      @logo1 = doc.css("shop").first.css("> logo_image").first.content
+      @logo1 = doc.css("shop")[0].css("> logo_image").first.content
       @logo2 = doc.css("shop")[1].css("> logo_image").first.content
       @logo3 = doc.css("shop")[2].css("> logo_image").first.content
       render "index"
