b6ba1edabf5dd30ecc17b3d49363aeb186071dfe
kai-hosoi <79440232+kai-hosoi@users.noreply.github.com>
2021-06-12 13:52:10 +0900

    質問数を増やしたことによりAPIの追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,12 @@ class SearchController < ApplicationController
   def index
     
   end
+
   def search
     area = params[:place]
 
     budget = params[:budget]
+
     if params[:style] == "0"
       style = URI.encode_www_form({night_view:1})
     else
@@ -43,16 +45,64 @@ class SearchController < ApplicationController
         genre = "G001"#居酒屋
       end
     end
-
+      #年と性格と予算と場所
       uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=29952817473475a8&large_area=#{area}&budget=#{budget}&#{style}&genre=#{genre}")
       xml = Net::HTTP.get(uri)
       doc = Nokogiri::XML(xml)
-      @name1 = doc.css("shop")[0].css("> name").first.content
+      @name1 = doc.css("shop").first.css("> name").first.content
       @name2 = doc.css("shop")[1].css("name").first.content
       @name3 = doc.css("shop")[2].css("name").first.content
-      @logo1 = doc.css("shop")[0].css("> logo_image").first.content
+      @logo1 = doc.css("shop").first.css("> logo_image").first.content
       @logo2 = doc.css("shop")[1].css("> logo_image").first.content
       @logo3 = doc.css("shop")[2].css("> logo_image").first.content
+      
+      if params[:number] == "1"
+        number = "G010"#各国料理
+      elsif params[:number] == "2"
+        number = "G002"#ダイニングバー・バル
+      elsif params[:number] == "3"
+        number = "G003"#創作料理
+      elsif params[:number] == "4"
+        number = "G004"#和食
+      elsif params[:number] == "5"
+        number = "G009"#アジア・エスニック料理
+      elsif params[:number] == "6"
+        number = "G006"#イタリアン・フレンチ
+      elsif params[:number] == "7"
+        number = "G007"#中華
+      elsif params[:number] == "8"
+        number = "G008"#焼肉・ホルモン
+      elsif params[:number] == "9"
+        number = "G013"#ラーメン
+      end
+
+      #数字と予算と場所
+      uri2 = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=29952817473475a8&large_area=#{area}&budget=#{budget}&genre=#{number}")
+      xml2 = Net::HTTP.get(uri2)
+      doc2 = Nokogiri::XML(xml2)
+      @name1_2 = doc2.css("shop")[0].css("> name").first.content
+      @name2_2 = doc2.css("shop")[1].css("name").first.content
+      @name3_2 = doc2.css("shop")[2].css("name").first.content
+      @logo1_2 = doc2.css("shop")[0].css("> logo_image").first.content
+      @logo2_2 = doc2.css("shop")[1].css("> logo_image").first.content
+      @logo3_2 = doc2.css("shop")[2].css("> logo_image").first.content
+
+
+
+
+
+
+      #季節と予算と場所
+      uri2 = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=29952817473475a8&large_area=#{area}&budget=#{budget}&genre=#{number}")
+      xml2 = Net::HTTP.get(uri2)
+      doc2 = Nokogiri::XML(xml2)
+      @name1_2 = doc2.css("shop")[0].css("> name").first.content
+      @name2_2 = doc2.css("shop")[1].css("name").first.content
+      @name3_2 = doc2.css("shop")[2].css("name").first.content
+      @logo1_2 = doc2.css("shop")[0].css("> logo_image").first.content
+      @logo2_2 = doc2.css("shop")[1].css("> logo_image").first.content
+      @logo3_2 = doc2.css("shop")[2].css("> logo_image").first.content
+      
   end
 
 end
