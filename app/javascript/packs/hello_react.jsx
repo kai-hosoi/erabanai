a1a2048c247db822f3fa4032458c437993e156c2
kai-hosoi <79440232+kai-hosoi@users.noreply.github.com>
2021-07-14 18:50:36 +0900

    reactiveに変更し、stateを受け継がせることができた

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -10,18 +10,13 @@ import  Routes  from './route';
 import FirstPage from './firstpage'
 import SecondPage from './secondpage'
 import Sample from './sample'
+import axios from 'axios'
 
 
 class Place extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {place:""};
-    this.handleChange = this.handleChange.bind(this);
     }
-  handleChange(event) {
-    this.setState({place: event.target.value});
-    console.log(this.state)
-  }
   render(){
     return(
       <div className = "container">
@@ -30,7 +25,7 @@ class Place extends React.Component{
               <div className="border rounded m-3 p-2">
               <label name="place">場所(駅名、地名を入力してください)</label>
                 <div>
-                  <input type="text" name="place" placeholder="入力してください" value={this.state.value} onChange={this.handleChange} required></input>
+                  <input type="text" name="place" placeholder="入力してください" value={this.props.place.value} onChange={this.props.handleChange} required></input>
                 </div>
                 <input type="button" value="次へ" onClick={() => this.props.State(this.props.num)}></input>
               </div>
@@ -45,6 +40,7 @@ class Budget extends React.Component{
   constructor(props) {
     super(props);
   }
+
   render(){
     return(
       <div className = "container">
@@ -54,17 +50,17 @@ class Budget extends React.Component{
               <label name="place">予算</label>
                 <div>
                   <label>
-                    <input type="radio" name="budget" vale="B002" required></input>3000円
+                    <input type="radio" name="budget" value="B002" checked={this.props.budget === 'B002'} onChange={this.props.onValueChange} required></input>3000円
                   </label>
                 </div>
                 <div>
                   <label>
-                    <input type="radio" name="budget" vale="B008" required></input>5000円
+                    <input type="radio" name="budget" value="B008" checked={this.props.budget === 'B008'} onChange={this.props.onValueChange} required></input>5000円
                   </label>
                 </div>
                 <div>
                   <label>
-                    <input type="radio" name="budget" vale="B005" required></input>10000円
+                    <input type="radio" name="budget" value="B005" checked={this.props.budget === 'B005'} onChange={this.props.onValueChange} required></input>10000円
                   </label>
                 </div>
                 <div>
@@ -91,12 +87,12 @@ class Budget extends React.Component{
                   <label naem="place">インドアですか？アウトドアですか？</label>
                     <div>
                       <label>
-                        <input type="radio" name="style" value="0" required></input>インドア派
+                        <input type="radio" name="style" value="0" checked={this.props.style === '0'} onChange={this.props.onValueChange} required></input>インドア派
                       </label>
                     </div>
                     <div>
                       <label>
-                        <input type="radio" name="style" value="1" required></input>アウトドア派
+                        <input type="radio" name="style" value="1" checked={this.props.style === '1'} onChange={this.props.onValueChange} required></input>アウトドア派
                       </label>
                     </div>
                     <input type="button" value="次へ" onClick={() => this.props.State(this.props.num)}></input>
@@ -121,17 +117,17 @@ class Budget extends React.Component{
                   <label name="place">年齢</label>
                   <div>
                     <label>
-                      <input type="radio" name="age" value="10" required></input>10-20代
+                      <input type="radio" name="age" value="10" checked={this.props.age === '10'} onChange={this.props.onValueChange} required></input>10-20代
                     </label>
                   </div>
                   <div>
                     <label>
-                      <input type="radio" name="age" value="30" required></input>30-40代
+                      <input type="radio" name="age" value="30" checked={this.props.age === '30'} onChange={this.props.onValueChange} required></input>30-40代
                     </label>
                   </div>
                   <div>
                     <label>
-                      <input type="radio" name="age" value="50" required></input>50-60代
+                      <input type="radio" name="age" value="50" checked={this.props.age === '50'} onChange={this.props.onValueChange} required></input>50-60代
                     </label>
                   </div>
                     <input type="submit" value="送信" ></input>
@@ -148,24 +144,47 @@ class Budget extends React.Component{
   class App extends React.Component{
     constructor(props) {
       super(props);
-      this.state = {num: 1};
+      this.state = {num: 1,place:"",budget:"",style:"",age:""};
+      this.handleChange = this.handleChange.bind(this);
+      this.onValueChange = this.onValueChange.bind(this);
     }
 
     onButtonClick = (state) => {
       this.setState({ num: state+1 });
     };
+    handleChange(event) {
+      this.setState({place: event.target.value});
+    }
   
+    onValueChange(event) {
+      this.setState({
+        budget: event.target.value
+      });
+    }
+
+    onValueChange2(event) {
+      this.setState({
+        style: event.target.value
+      });
+    }
+
+    onValueChange3(event) {
+      this.setState({
+        age: event.target.value
+      });
+    }
 
     render(){
-      console.log(this.state.num)
-      console.log(data)
-      const { num } = this.state
+      console.log(this.state)
+      const { num,place,budget,style,age} = this.state
+      let simpleDataJSON = JSON.stringify(this.state);
+      console.log(simpleDataJSON);
       return(
         <div>
-          {num === 1 && <Place State={(state) => this.onButtonClick(state)} num={num}/>}
-          {num === 2 && <Budget State={(state) => this.onButtonClick(state)} num={num}/>}
-          {num === 3 && <Style State={(state) => this.onButtonClick(state)} num={num}/>}
-          {num === 4 && <Age State={(state) => this.onButtonClick(state)} num={num}/>}
+          {num === 1 && <Place State={(state) => this.onButtonClick(state)} num={num} place={place} handleChange={(event) => this.handleChange(event)}/>}
+          {num === 2 && <Budget State={(state) => this.onButtonClick(state)} num={num} budget={budget} onValueChange={(event) =>this.onValueChange(event)}/>}
+          {num === 3 && <Style State={(state) => this.onButtonClick(state)} num={num} style={style} onValueChange={(event) =>this.onValueChange2(event)}/>}
+          {num === 4 && <Age State={(state) => this.onButtonClick(state)} num={num} age={age} onValueChange={(event) =>this.onValueChange3(event)}/>}
         </div>
       )
     }
@@ -178,9 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(document.createElement('div')),
   )
 })
-var textbox = document.getElementById("place")
-var data = {
-  place: textbox.value
-}
+
+
 
 
